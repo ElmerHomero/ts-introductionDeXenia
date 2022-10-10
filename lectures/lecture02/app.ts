6085c164f88b45ee7a4ebbba457b6b2e59e670e2
Xenia Padilla <xeniapadillamadrid@gmail.com>
2022-10-09 20:32:31 -0700

    lecture 02

---
+++ lectures/lecture02/app.ts
@@ -0,0 +1,38 @@
+/**
+ * Lecture 02
+ * Author: ME Xenia Padilla
+ * Date: 9 oct 2022
+ * Title: Introduction to Arrays
+ * Objetive: Learn how you can use Arrays in TypeScript
+ * 
+ * Tip: For compile your file, you will need to use the command: tsc fileName.ts
+ */
+
+
+/**
+ *  In TS, like JS, has arrays of values. The array type can be defined in one of two ways. 
+ * 
+ *  The first is to denote tye type of the array elements before []:
+ * 
+ *  let list: ages[] = [24, 56, 40];
+ * 
+ *  The second way is to use the generalization Array<elemType>
+ * 
+ *  let list: Array<number> = [24, 56, 40];
+ */
+
+
+/** 
+ *  Your mission is:
+ * 
+ * 1. Declare an Array named: peopleNames
+ * 1.1 Fille the array with 5 names
+ * 1.2 Print in console the whole array
+ * 1.3 Use the differents methods to iterate an Array
+ * 1.3.1 for
+ * 1.3.2 forin
+ * 1.3.3 forof
+ * 1.3.4 map
+ * 1.3.5 filter
+ * 1.3.6 reduce (if apply)
+ */
